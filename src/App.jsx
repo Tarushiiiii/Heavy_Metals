713298578ad98937f7f4fccaab77c7bfefc76a43
Yoshita09 <yoshitasinghal.2005@gmail.com>
2025-09-30 15:25:58 +0530

    subtabs

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Map } from './pages/map';
 import { CalculateHMPI } from './pages/data_entry';
 import { Settings } from './pages/settings';
 import { Analysis } from './pages/analysis';
+import { Methodology } from './components/analysis/methodology';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
       // { path: "data_entry", element: <DataEntry />, handle: { header: 'Data Entry' } },
       { path: "calculate_hmpi", element: <CalculateHMPI />, handle: { header: 'Calculate HMPI' } },
       { path: "analysis", element: <Analysis />, handle: { header: 'Analysis' } },
+      { path: "methodology", element: <Methodology />, handle: { header: 'Methodology' } },
       { path: "settings", element: <Settings />, handle: { header: 'Settings' } },
       { path: "*", element: <h1>Not Found</h1> },
     ]
